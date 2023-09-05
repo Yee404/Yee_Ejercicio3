daca015438f14787aa8c3350b7107a48b646a909
Yee404 <131786086+Yee404@users.noreply.github.com>
2023-09-05 17:43:32 -0600

    Add files via upload

---
+++ Yee_Ejercicio3/Examenes.java
@@ -0,0 +1,11 @@
+public class Examenes{
+    String examen;
+
+    public Examenes(String examen){
+        this.examen;
+    }
+
+    public String getExamen(){
+        return this.examen;
+    }
+}
